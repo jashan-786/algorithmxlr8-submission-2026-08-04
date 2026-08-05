4c5507099585e92d72e7e9164933eedc549533f6
jashan-786 <91558452+jashan-786@users.noreply.github.com>
2026-08-04 20:28:07 -0500

    Solve Sum of All Odd Length Subarrays

--- AlgorithmXlr8-P006-SumOfAllOddLengthSubarrays/Solution.cpp
+++ AlgorithmXlr8-P006-SumOfAllOddLengthSubarrays/Solution.cpp
@@ -7,27 +7,47 @@ int main() {
     vector<long long> nums(n);
     for (int i = 0; i < n; i++) cin >> nums[i];
 
+
+            // first wya 
+
+//             int leftOdd= (left)/2;
+//             int leftEven= (left+1)/2;
+
+//             int rightOdd= (right)/2;
+//             int rightEven=(right+1)/2;
+
+// // left + 1 right  if we remove 1
+//             total += nums[i] * (leftEven * rightEven 
+//        + leftOdd * rightOdd);
+// second way 
+
+
+// count total and odd if number is odd dived by 2 +1 if even half going to be odd forsure
+    
+    // for everyt index count left odd and even + 
+            // right odd and even
     
     int total=0;
-    for(int i=0; i< n; i++){
+    for(int i=0; i< n; i++)
+    {
 
-            // for everyt index count left odd and even + 
-            // right odd and even
+            
 
             int left= i+1;
             int right=n-i;
 
-            int leftOdd= (left)/2;
-            int leftEven= (left+1)/2;
 
-            int rightOdd= (right)/2;
-            int rightEven=(right+1)/2;
+    
+    int totalc= left * right;
 
-// left + 1 right  if we remove 1
-            total += nums[i] * (leftEven * rightEven 
-       + leftOdd * rightOdd);
+    int odd= (totalc/2);
+
+    if(totalc %2 != 0){
+        odd++;
     }
 
+    total= total + nums[i] * odd;
+    }
 
     cout << total << " ";
     return 0;
