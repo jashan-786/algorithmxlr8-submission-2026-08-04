8e4e470dfcb46d96a99a7501fc1cb78fb6af8ced
jashan-786 <91558452+jashan-786@users.noreply.github.com>
2026-08-07 23:45:07 -0500

    Solve Subarray Sum Equals K

---
+++ AlgorithmXlr8-P007-SubarraySumEqualsK/Solution.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    long long k;
+    cin >> n >> k;
+    vector<long long> nums(n);
+    for (int i = 0; i < n; i++) cin >> nums[i];
+
+    // Write your solution here.
+    // Print the total number of contiguous subarrays whose sum equals 
+    map <long long, long long> mp;
+    mp[0]=1;
+    long long  sum=0;
+    int count=0;
+    for(int i=0;  i< n;  i++){
+        sum+= nums[i];
+        long long checkInMap= sum - k;
+
+        if (mp.find(checkInMap) != mp.end()) {
+        count += mp[checkInMap];
+        
+    
+    }
+        
+        mp[sum]++;
+        
+
+
+    }
+
+    cout << count << "";
+
+    return 0;
+}
