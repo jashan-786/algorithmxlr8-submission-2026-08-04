c04c76fe7ebb395efb8b114d7fec5456959264ad
jashan-786 <91558452+jashan-786@users.noreply.github.com>
2026-08-10 19:55:20 -0500

    Solve Count Number of Nice Subarrays

--- AlgorithmXlr8-P012-CountNumberOfNiceSubarrays/Solution.cpp
+++ AlgorithmXlr8-P012-CountNumberOfNiceSubarrays/Solution.cpp
@@ -7,32 +7,63 @@ int main() {
     vector<int> nums(n);
     for (int i = 0; i < n; i++) cin >> nums[i];
 
-    // Write your solution here.
-    // Print the number of subarrays containing exactly k odd numbers.
-    int ans=0;
-
-    for(int i=0; i < n ; i++){
 
 
+    int currSum=0;
+    map<int, int>mp;
 
-        int pointer=i;
-        int odd=0;
-        int tempsum=0;
-        int counter =0;
-        while ( pointer < n ){
-            if(nums[pointer] %2 != 0){
-                odd++;
+    mp[0]=1;
+    int output=0;
+    int oddCounter=0;
+    for(int i=0; i< n; i++){
+
+
+            if( nums[i] %2 != 0){
+            currSum++;
             }
 
-            pointer++;
-             if(odd == k){
-                ans++;
-        }
-        }
+            int prefixToBeCheckedBefore= currSum - k;
 
-       
+            if(mp.find(prefixToBeCheckedBefore) != mp.end()){
+
+                output+= mp[prefixToBeCheckedBefore];
+
+            }
+
+            mp[currSum]++;
+
     }
 
-    cout << ans <<"";
+    cout << output << "";
     return 0;
+
+    // Write your solution here.
+    // Print the number of subarrays containing exactly k odd numbers.
+    // int ans=0;
+
+    // for(int i=0; i < n ; i++){
+
+
+
+    //     int pointer=i;
+    //     int odd=0;
+    //     int tempsum=0;
+    //     int counter =0;
+    //     while ( pointer < n ){
+    //         if(nums[pointer] %2 != 0){
+    //             odd++;
+    //         }
+
+    //         pointer++;
+    //          if(odd == k){
+    //             ans++;
+    //     }
+    //     }
+
+       
+    // }
+
+    // cout << ans <<"";
+
+
 }
