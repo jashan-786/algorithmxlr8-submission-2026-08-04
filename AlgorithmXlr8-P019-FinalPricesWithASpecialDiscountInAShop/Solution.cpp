f85d2099dcf56874b6638ee1dd6830506828c735
jashan-786 <91558452+jashan-786@users.noreply.github.com>
2026-08-29 13:12:22 -0500

    Solve Final Prices With a Special Discount in a Shop

--- AlgorithmXlr8-P019-FinalPricesWithASpecialDiscountInAShop/Solution.cpp
+++ AlgorithmXlr8-P019-FinalPricesWithASpecialDiscountInAShop/Solution.cpp
@@ -13,24 +13,59 @@ int main() {
     // non-decreasing stack of indices resolves all discounts in one
     // pass. Print the final prices space-separated.
 
-       
-        vector <int> answer(n);
+
+     vector <int> answer(n);
+     stack <pair<int,int>> st;
 
 
-        for(int i=0; i< n ;i++){
+    for( int i=0; i< n ; i++){
 
-                int curDiscount=0;
-             for( int j= i+1; j<n ; j++ ){
-                if(prices[j] <= prices[i]){
-                        curDiscount= prices[j];
-                    break;
-                }
+     
+     if(st.empty()){
+        st.push({i , prices[i]});
+     }else{
 
-             }
+        if(!st.empty() && st.top().second < prices[i]){
+            st.push({i , prices[i]});
+
+        }
+        else{
+        while(!st.empty() && st.top().second >= prices[i]){
+            auto p= st.top();
+            answer[p.first]= p.second - prices[i];
+            st.pop();
+        }
+
+        st.push({i,prices[i]});
+        }
+     }
+
+    }
+
+    while(! st.empty()){
+            auto p= st.top();
+            answer[p.first]= p.second ;
+            st.pop();
+
+    }
+               
+        // vector <int> answer(n);
 
 
-            answer[i]= prices[i] - curDiscount;
-        } 
+        // for(int i=0; i< n ;i++){
+
+        //         int curDiscount=0;
+        //      for( int j= i+1; j<n ; j++ ){
+        //         if(prices[j] <= prices[i]){
+        //                 curDiscount= prices[j];
+        //             break;
+        //         }
+
+        //      }
+
+
+        //     answer[i]= prices[i] - curDiscount;
+        // } 
 
         for(int i=0; i < n ; i++){
             cout << answer[i] << " ";
